5c4ff9bc34cc4238500161359d4240ab87a6e691
Yash Sharma <yashssharma2006@gmail.com>
2026-09-19 16:12:41 +0530

    admin user management, basically admin can see al users

--- backend/routers/admin.py
+++ backend/routers/admin.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
+from database.database import get_db
 from models.user import User as UserModel
 from utils.dependencies import require_admin
 
@@ -10,10 +12,17 @@ router = APIRouter(
 
 @router.get("/users")
 def get_all_users(
-    current_user: UserModel = Depends(require_admin)
+    current_user: UserModel = Depends(require_admin),
+    db: Session = Depends(get_db)
 ):
-    return {
-        "message": "Welcome Admin",
-        "admin": current_user.email
-    }
+    users = db.query(UserModel).all()
 
+    return[
+        {
+        "id": user.id,
+        "name": user.name,
+        "email": user.email,
+        "role": user.role
+        }
+        for user in users
+    ]
